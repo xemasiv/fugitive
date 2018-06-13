1a00ffbd927c655f7b22b517618d52f168689ad7
“xemasiv” <xemasiv@gmail.com>
2018-06-13 21:26:33 +0800

    ASDASDASDASDASDASD

--- src/fugitive.client.js
+++ src/fugitive.client.js
@@ -2,10 +2,99 @@ import pako from 'pako';
 import msgpack5 from 'msgpack5';
 import SimplePeer from 'simple-peer';
 import Peer from 'simple-peer';
+import mitt from 'mitt';
 import createStore from 'unistore';
 import { sha224 } from 'js-sha256';
 import md5 from 'js-md5';
 import PouchDB from 'pouchdb-browser';
+import debug from 'debug';
 
-const debug = false;
-const socket = new WebSocket('ws://127.0.0.1/');
+const log = debug('client');
+log.enabled = true;
+
+const msgpack = msgpack5();
+
+class FugitiveClient {
+  constructor () {
+    const self = this;
+
+    const target_rtc_count = 1;
+
+    const emitter = mitt();
+    const on = emitter.on.bind(emitter);
+    const emit = emitter.emit.bind(emitter);
+    const off = emitter.off.bind(emitter);
+    self.on = on;
+    self.emit = emit;
+    self.off = off;
+
+    const rtcs = [];
+
+    let ws = new WebSocket('ws://127.0.0.1/');
+    ws.binaryType = "arraybuffer";
+    let send = (m) => ws.send(msgpack.encode(m));
+
+    ws.onopen = () => {
+      self.ws = ws;
+      log('connected');
+      if (rtcs.length < target_rtc_count) {
+        send({ status: 'seeking' });
+      }
+    };
+    let rtc;
+    ws.onmessage = ({ data: m }) => {
+      const { status, offer, answer } = msgpack.decode(m);
+      log('received:', status);
+      switch (status) {
+        case 'welcome':
+          log('welcomed by server');
+          break;
+        case 'awaiting_offer':
+          log('awaiting offer..');
+          break;
+        case 'creating_offer':
+          rtc = new SimplePeer({
+            initiator: true
+          });
+          rtc.on('signal', (offer) => {
+            send({
+              status: 'sending_offer',
+              offer
+            });
+          });
+          rtc.on('connect', () => {
+            log('alice connected');
+          });
+          break;
+        case 'receiving_offer':
+          rtc = new SimplePeer({
+            initiator: false
+          });
+          rtc.signal(offer);
+          rtc.on('signal', (answer) => {
+            send({
+              status: 'sending_answer',
+              answer
+            });
+          });
+          rtc.on('connect', () => {
+            log('bob connected');
+          });
+          break;
+        case 'receiving_answer':
+          rtc.signal(answer);
+          break;
+        default:
+          log('unhandled:', status);
+          break;
+      }
+    };
+    ws.onclose = () => {
+      self.ws = undefined;
+    };
+    ws.onerror = console.error;
+  }
+}
+
+const Client = new FugitiveClient();
+Client.on('connected', console.log)
--- src/fugitive.server.js
+++ src/fugitive.server.js
@@ -1,10 +1,65 @@
-const WebSocketServer = require('uws').Server;
+import msgpack5 from 'msgpack5';
+import uws from 'uws';
+import debug from 'debug';
+
+const log = debug('server');
+log.enabled = true;
+
+const msgpack = msgpack5();
+
+const WebSocketServer = uws.Server;
 const wss = new WebSocketServer({ port: 80 });
+
+const seeking = [];
+const pairs = [];
+const serializer = (ws) => (m) => ws.send(msgpack.encode(m));
 wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log('Received: ' + message);
-    ws.send(`Reply from ${process.pid}`);
+  const send = serializer(ws);
+  ws.on('message', (m) => {
+    const { status, offer, answer } = msgpack.decode(m);
+    let alice, bob, pair;
+    switch (status) {
+      case 'seeking':
+        seeking.push(ws);
+        if (seeking.length === 2) {
+          log('pair found');
+          alice = seeking.shift();
+          bob = seeking.shift();
+          serializer(alice)({
+            ur: 'alice',
+            status: 'creating_offer'
+          });
+          serializer(bob)({
+            ur: 'bob',
+            status: 'awaiting_offer'
+          });
+          pairs.push({ alice, bob });
+        }
+        break;
+      case 'sending_offer':
+        pair = pairs.filter((pair) => {
+          return pair.alice === ws;
+        })[0];
+        serializer(pair.bob)({
+          status: 'receiving_offer',
+          offer
+        });
+        break;
+      case 'sending_answer':
+        pair = pairs.filter((pair) => {
+          return pair.bob === ws;
+        })[0];
+        serializer(pair.alice)({
+          status: 'receiving_answer',
+          answer
+        });
+        break;
+      default:
+        log('unhandled:', status);
+        break;
+    }
   });
-  ws.send(`Welcome from ${process.pid}`);
+  send({ status: 'welcome' });
+
 });
-console.log('listening on port 80');
+log('listening on port 80');
