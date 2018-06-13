d2e842b243f2db968d71e347438bb1348b3d23a0
“xemasiv” <xemasiv@gmail.com>
2018-06-14 00:35:01 +0800

    bnguguufghfghfgh

--- src/fugitive.client.js
+++ src/fugitive.client.js
@@ -29,10 +29,19 @@ class FugitiveClient {
     const target_rtc_count = 1;
 
     const rtcs = [];
+    self.rtcs = rtcs;
 
     let ws = new WebSocket('ws://127.0.0.1/');
     ws.binaryType = "arraybuffer";
+
     let send = (m) => ws.send(msgpack.encode(m));
+    self.send = ((m) => {
+      log('multicasting', m);
+      rtcs.map((rtc) => {
+        log('unicasting', m);
+        rtc.send(msgpack.encode(m));
+      });
+    });
 
     ws.onopen = () => {
       self.ws = ws;
@@ -42,6 +51,24 @@ class FugitiveClient {
       }
     };
     let rtc;
+    const resolver = (rtc) => (data) => {
+      const { action, url, content } = msgpack.decode(data);
+      log(action, url, content);
+      switch (action) {
+        case 'request':
+          if (lru.has(url)) {
+            content = lru.get(url);
+            rtc.send(
+              msgpack.encode({
+                action: 'resolve', url, content
+              })
+            );
+          }
+          break;
+        case 'resolve':
+          break;
+      }
+    };
     ws.onmessage = ({ data: m }) => {
       const { status, offer, answer } = msgpack.decode(m);
       log('received:', status);
@@ -53,10 +80,7 @@ class FugitiveClient {
           log('awaiting offer..');
           break;
         case 'creating_offer':
-          rtc = new SimplePeer({
-            initiator: true,
-            trickle: true
-          });
+          rtc = new SimplePeer({ initiator: true });
           rtc.on('signal', (offer) => {
             send({
               status: 'sending_offer',
@@ -65,13 +89,14 @@ class FugitiveClient {
           });
           rtc.on('connect', () => {
             log('alice connected');
+            rtcs.push(rtc);
+            rtc.send('hello');
           });
+          rtc.on('data', resolver(rtc));
+          rtc.on('error', console.error);
           break;
         case 'receiving_offer':
-          rtc = new SimplePeer({
-            initiator: false,
-            trickle: true
-          });
+          rtc = new SimplePeer();
           rtc.signal(offer);
           rtc.on('signal', (answer) => {
             send({
@@ -81,7 +106,11 @@ class FugitiveClient {
           });
           rtc.on('connect', () => {
             log('bob connected');
+            rtcs.push(rtc);
+            rtc.send('hello');
           });
+          rtc.on('data', resolver(rtc));
+          rtc.on('error', console.error);
           break;
         case 'receiving_answer':
           rtc.signal(answer);
@@ -101,6 +130,7 @@ class FugitiveClient {
     return new Promise((resolve, reject) => {
       let controller = new AbortController();
       opts.signal = controller.signal;
+      self.send({ action: 'request', url });
       fetch(url, opts)
         .then((response) => {
           if (response.ok) return response.blob();
@@ -109,12 +139,13 @@ class FugitiveClient {
           );
         })
         .then((blob) => {
-          let f = new FileReader();
-          f.readAsArrayBuffer(blob);
-          f.onloadend = () => {
-            let h = sha224(f.result);
-            self.lru.set(h, blob);
-          };
+          // let f = new FileReader();
+          // f.readAsArrayBuffer(blob);
+          // f.onloadend = () => {
+          //   let h = sha224(f.result);
+          //   self.lru.set(h, blob);
+          // };
+          self.lru.set(url, blob);
           let u = URL.createObjectURL(blob);
           resolve(u);
         })
@@ -130,3 +161,4 @@ class FugitiveClient {
 
 const Client = new FugitiveClient();
 window.Client = Client;
+window.msgpack = msgpack;
